585ca62db5bd6cb7e011f4599ca992949f8d1f8b
gwanwoochoi <cgw1999@unist.ac.kr>
2025-03-12 22:23:09 +0900

    Modify code as runnable

--- code_implementation/dqn.py
+++ code_implementation/dqn.py
@@ -17,8 +17,6 @@ from stable_baselines3.common.buffers import ReplayBuffer
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import trange
 
-run_name = None
-
 
 @dataclass
 class Args:
@@ -28,17 +26,15 @@ class Args:
     """seed of the experiment"""
     torch_deterministic: bool = True
     """if toggled, `torch.backends.cudnn.deterministic=False`"""
-    cuda: bool = True
-    """if toggled, cuda will be enabled by default"""
-    track: bool = False
+    track: bool = True
     """if toggled, this experiment will be tracked with Weights and Biases"""
-    wandb_project_name: str = "cleanRL"
+    wandb_project_name: str = "dqn-acrobot"
     """the wandb's project name"""
     wandb_entity: str = None
     """the entity (team) of wandb's project"""
-    capture_video: bool = False
+    capture_video: bool = True
     """whether to capture videos of the agent performances (check out `videos` folder)"""
-    save_model: bool = False
+    # save_model: bool = True
     """whether to save model into the `runs/{run_name}` folder"""
     upload_model: bool = False
     """whether to upload the saved model to huggingface"""
@@ -46,15 +42,16 @@ class Args:
     """the user or org name of the model repository from the Hugging Face Hub"""
 
     # Algorithm specific arguments
-    env_id: str = "CartPole-v1"
+    env_id: str = "Acrobot-v1"
+    # env_id: str = "CartPole-v1"
     """the id of the environment"""
     total_timesteps: int = 500000
     """total timesteps of the experiments"""
-    learning_rate: float = 2.5e-4
+    learning_rate: float = 3e-4
     """the learning rate of the optimizer"""
     num_envs: int = 1
     """the number of parallel game environments"""
-    buffer_size: int = 10000
+    buffer_size: int = 50000
     """the replay memory buffer size"""
     gamma: float = 0.99
     """the discount factor gamma"""
@@ -62,7 +59,7 @@ class Args:
     """the target network update rate"""
     target_network_frequency: int = 500
     """the timesteps it takes to update the target network"""
-    batch_size: int = 128
+    batch_size: int = 256
     """the batch size of sample from the reply memory"""
     start_e: float = 1
     """the starting epsilon for exploration"""
@@ -70,25 +67,37 @@ class Args:
     """the ending epsilon for exploration"""
     exploration_fraction: float = 0.5
     """the fraction of `total-timesteps` it takes from start-e to go end-e"""
-    learning_starts: int = 10000
+    learning_starts: int = 50000
     """timestep to start learning"""
-    train_frequency: int = 10
+    train_frequency: int = 50
     """the frequency of training"""
+    save_frequency: int = 50000
+    """the frequency of training model saving"""
+    logging_frequency: int = 10
 
 
-def make_env(env_id, seed, idx, capture_video, run_name):
-    def thunk():
-        if capture_video and idx == 0:
-            env = gym.make(env_id, render_mode="rgb_array")
-            env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
-        else:
-            env = gym.make(env_id)
-        env = gym.wrappers.RecordEpisodeStatistics(env)
-        env.action_space.seed(seed)
+def make_train_env_list(args):
+    def thunk(idx):
+        def _thunk():
+            if args.capture_video and idx == 0:
+                env = gym.make(args.env_id, render_mode="rgb_array")
+                env = gym.wrappers.RecordVideo(
+                    env,
+                    video_folder=f"runs/{run_name}/videos",
+                    video_length=1000,
+                    step_trigger=lambda step: step % args.save_frequency == 0,
+                )
+            else:
+                env = gym.make(args.env_id)
+            env = gym.wrappers.RecordEpisodeStatistics(env)
+            env.action_space.seed(args.seed)
 
-        return env
+            return env
 
-    return thunk
+        return _thunk
+
+    env_callable_list = [thunk(i) for i in range(args.num_envs)]
+    return env_callable_list
 
 
 # ALGO LOGIC: initialize agent here:
@@ -118,7 +127,7 @@ if __name__ == "__main__":
     if args.track:
         import wandb
 
-        wandb.init(
+        run = wandb.init(
             project=args.wandb_project_name,
             entity=args.wandb_entity,
             sync_tensorboard=True,
@@ -127,22 +136,17 @@ if __name__ == "__main__":
             monitor_gym=True,
             save_code=True,
         )
-    writer = SummaryWriter(f"runs/{run_name}")
-    writer.add_text(
-        "hyperparameters",
-        "|param|value|\n|-|-|\n%s"
-        % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
-    )
+        # wandb.define_metric("train/step")
+        # wandb.define_metric("train/*", step_metric="train/step")
 
     # TRY NOT TO MODIFY: seeding
     seed_setting(args.seed, args.torch_deterministic)
 
     # env setup
     envs = gym.vector.SyncVectorEnv(
-        [
-            make_env(args.env_id, args.seed + i, i, args.capture_video, run_name)
-            for i in range(args.num_envs)
-        ]
+        make_train_env_list(args)
+        # make_env(args.env_id, args.seed + i, i, args.capture_video, run_name)
+        # for i in range(args.num_envs)
     )
     assert isinstance(envs.single_action_space, gym.spaces.Discrete), (
         "only discrete action space is supported"
@@ -158,13 +162,14 @@ if __name__ == "__main__":
         envs.single_observation_space,
         envs.single_action_space,
         device,
+        args.num_envs,
         handle_timeout_termination=False,
     )
     start_time = time.time()
 
     # TRY NOT TO MODIFY: start the game
     obs, _ = envs.reset(seed=args.seed)
-    for global_step in range(args.total_timesteps):
+    for global_step in trange(args.total_timesteps):
         # ALGO LOGIC: put action logic here
         epsilon = linear_schedule(
             start_e=args.start_e,
@@ -183,73 +188,76 @@ if __name__ == "__main__":
         # TRY NOT TO MODIFY: execute the game and log data.
         next_obs, rewards, terminations, truncations, infos = envs.step(actions)
 
-        # TRY NOT TO MODIFY: record rewards for plotting purposes
-        if "final_info" in infos:
-            for info in infos["final_info"]:
-                if info and "episode" in info:
-                    print(
-                        f"global_step={global_step}, episodic_return={info['episode']['r']}"
-                    )
-                    writer.add_scalar(
-                        "charts/episodic_return", info["episode"]["r"], global_step
-                    )
-                    writer.add_scalar(
-                        "charts/episodic_length", info["episode"]["l"], global_step
-                    )
-
         # TRY NOT TO MODIFY: save data to reply buffer; handle `final_observation`
         real_next_obs = next_obs.copy()
-        for idx, trunc in enumerate(truncations):
-            if trunc:
-                real_next_obs[idx] = infos["final_observation"][idx]
-        rb.add(obs, real_next_obs, actions, rewards, terminations, infos)
+        # for idx, trunc in enumerate(truncations):
+        # if trunc:
+        #     real_next_obs[idx] = infos["final_observation"][idx]
+        if actions.ndim == 1:
+            actions = actions[:, None]
+        not_trunc = True
+        for idx, truc in enumerate(truncations):
+            if truc:
+                not_trunc = False
+        if not_trunc:
+            rb.add(obs, real_next_obs, actions, rewards, terminations, infos)
 
         # TRY NOT TO MODIFY: CRUCIAL step easy to overlook
         obs = next_obs
 
         # ALGO LOGIC: training.
         if global_step > args.learning_starts:
+            if global_step % args.save_frequency == 0:
+                model_path = f"runs/{run_name}/{global_step}.pth"
+                torch.save(q_network.state_dict(), model_path)
+
             if global_step % args.train_frequency == 0:
                 data = rb.sample(args.batch_size)
-                pass
-                # q_values = q_network(data.observations.to(device))
+                q_values = q_network(data.observations.to(device))
+                q_values = torch.gather(q_values, dim=1, index=data.actions)
+
+                with torch.no_grad():
+                    target_max_q_values = target_network(
+                        data.next_observations.to(device)
+                    )
+                    target_max_q_values = target_max_q_values.max(
+                        dim=1, keepdim=True
+                    ).values
+
+                targets = data.rewards + args.gamma * target_max_q_values
+                loss = F.mse_loss(q_values, targets)
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+
+            if "episode" in infos and args.track:
+                wandb.log(
+                    data={
+                        "train/episodic_return": infos["episode"]["r"],
+                        "train/episodic_length": infos["episode"]["l"],
+                        "train/lr": optimizer.param_groups[0]["lr"],
+                    }
+                )
 
             # update target network
             if global_step % args.target_network_frequency == 0:
-                pass
+                target_network.load_state_dict(q_network.state_dict())
 
-    if args.save_model:
-        model_path = f"runs/{run_name}/{args.exp_name}.cleanrl_model"
-        torch.save(q_network.state_dict(), model_path)
-        print(f"model saved to {model_path}")
-        from cleanrl_utils.evals.dqn_eval import evaluate
-
-        episodic_returns = evaluate(
-            model_path,
-            make_env,
-            args.env_id,
-            eval_episodes=10,
-            run_name=f"{run_name}-eval",
-            Model=QNetwork,
-            device=device,
-            epsilon=0.05,
-        )
-        for idx, episodic_return in enumerate(episodic_returns):
-            writer.add_scalar("eval/episodic_return", episodic_return, idx)
-
-        if args.upload_model:
-            from cleanrl_utils.huggingface import push_to_hub
-
-            repo_name = f"{args.env_id}-{args.exp_name}-seed{args.seed}"
-            repo_id = f"{args.hf_entity}/{repo_name}" if args.hf_entity else repo_name
-            push_to_hub(
-                args,
-                episodic_returns,
-                repo_id,
-                "DQN",
-                f"runs/{run_name}",
-                f"videos/{run_name}-eval",
-            )
+    model_path = f"runs/{run_name}/latest.pth"
+    torch.save(q_network.cpu().state_dict(), model_path)
+    print(f"model saved to {model_path}")
 
     envs.close()
-    writer.close()
+    # if args.upload_model:
+    #     from cleanrl_utils.huggingface import push_to_hub
+
+    #     repo_name = f"{args.env_id}-{args.exp_name}-seed{args.seed}"
+    #     repo_id = f"{args.hf_entity}/{repo_name}" if args.hf_entity else repo_name
+    #     push_to_hub(
+    #         args,
+    #         episodic_returns,
+    #         repo_id,
+    #         "DQN",
+    #         f"runs/{run_name}",
+    #         f"videos/{run_name}-eval",
+    #     )
